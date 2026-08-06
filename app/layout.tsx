3542c10dfb4b7ba1a3bb15528aa893186320b6eb
Brahim Amdouy <amdouy.brahim@gmail.com>
2026-08-06 11:21:13 +0100

    feat: intégration Google Analytics 4 (GA4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Raleway, DM_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const raleway = Raleway({
@@ -379,7 +380,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MY55780SP1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MY55780SP1');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
